9e4762be0b0920fa4636de1c6171fdc2cc14f42c
AjayKumar0220 <ajaykumar.madadha@hotmail.com>
2024-12-15 05:41:03 +0000

    Update web app script

--- script.sh
+++ script.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 echo "Deploying Web Apps On NGINX"
-echo "Deploying Done On NGINX"
+
+echo "Updating Syatem"
+sudo apt update -y
+
+echo "Install Utilities"
+sudo apt install -y zip unzip
+
+echo "Install NGINX"
+sudo apt install -y nginx
+
+echo "Remove old Files"
+sudo rm -r /var/www/html
+
+echo "Deploy Login Page"
+sudo git clone https://github.com/AjayKumar0220/Login-Page.git /var/www/html
+
+echo "Deployed Web App On NGINX"
